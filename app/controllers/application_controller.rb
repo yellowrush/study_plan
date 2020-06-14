8ec1ddbc0b3ee192a805258b2a7519caba1704c6
Tom.Huang <>
2020-06-14 23:39:46 +0900

    学習

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   include Clearance::Controller
+
+  def current_controller?(names)
+    names.include?(params[:controller]) unless params[:controller].blank? || false
+  end
+
+  helper_method :current_controller?
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,29 +10,29 @@ class RecipesController < ApplicationController
   end
 
   def new
-    @recipes = current_user.recipes.build
-  end
-
-  def create
-    @recipe = current_user.recipes.build(recipe_params)
-
-    if @recipe.save
-      redirect_to recipe_path(@recipe), notice: "Recipe Created!"
-    else
-      @errors = @recipe.errors.full_messages
-      render :new
-    end
+    @recipe = current_user.recipes.build
   end
 
   def edit
     @recipe = current_user.recipes.find(params[:id])
   end
 
+  def create
+    @recipe = current_user.recipes.build(recipe_params)
+
+    if @recipe.save
+      redirect_to recipe_path(@recipe), notice: "新規予定作成しました!"
+    else
+      @errors = @recipe.errors.full_messages
+      render :new
+    end
+  end
+
   def update
     @recipe = current_user.recipes.find(params[:id])
 
     if @recipe.update_attributes(recipe_params)
-      redirect_to recipe_path(@recipe), notice: "Recipe Updated!"
+      redirect_to recipe_path(@recipe), notice: "進度予定更新しました!"
     else
       @errors = @recipe.errors.full_messages
       render :edit
@@ -40,14 +40,17 @@ class RecipesController < ApplicationController
   end
 
   def destroy
-    recipe = current_user.recipes.find(params[:id])
-    recipe.destroy
-    redirect_to recipes_path, notice: "Deleted Recipe: #{recipe.name}"
+    @recipe = current_user.recipes.find(params[:id])
+    if @recipe.destroy
+      redirect_to recipes_path, notice: "#{@recipe.name}を削除しました!"
+    else
+      @errors = @recipe.errors.full_messages
+      render :index
+    end
   end
 
   private
-
-  def recipe_params
-    params.require(:recipe).permit(:name, :description)
-  end
+    def recipe_params
+      params.require(:recipe).permit(:name, :description)
+    end
 end
